6a58ddb565722975ab6d38c781c1d75113c8ffd7
Jess <janchrominski@gmail.com>
2025-11-16 00:19:38 +1030

    Binary Tree - Insert

---
+++ src/binary_tree.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self, value: int):
+        self.value: int = value
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root: Node = None
+
+    def insert(self, value: int) -> None:
+        """
+        O complexity space: O(1)
+        O complexity time: O(log n)
+        """
+        new_node = Node(value)
+
+        if not self.root:
+            self.root = new_node
+            return
+
+        # Start of tree traversal
+        current_node = self.root
+
+        while value != current_node.value:
+            if value < current_node.value:
+                if not current_node.left:
+                    current_node.left = new_node
+
+                # Proceed to the next level
+                current_node = current_node.left
+            else:
+                if not current_node.right:
+                    current_node.right = new_node
+
+                # Proceed to the next level
+                current_node = current_node.right
---
+++ test/test_tree.py
@@ -0,0 +1,31 @@
+from src.binary_tree import BinarySearchTree, Node
+import random
+
+def is_valid_bst(node: Node,
+                 min_val: float=float("-inf"),
+                 max_val: float=float("inf")) -> bool:
+    if node is None:
+        return True
+    if not (min_val < node.value < max_val):
+        return False
+    return (
+        # Testing Right
+        is_valid_bst(node=node.left,
+                     min_val=min_val,
+                     max_val=node.value) and
+        # Testing Left
+        is_valid_bst(node=node.right,
+                     min_val=node.value,
+                     max_val=max_val)
+    )
+
+def test_tree() -> None:
+    # Arrange
+    tree = BinarySearchTree()
+
+    # Act
+    for _ in range(20):
+        tree.insert(random.randint(0,50))
+
+    # Assert
+    assert is_valid_bst(tree.root)
